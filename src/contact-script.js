4f63b402e291536b409ed399b8dc47aaf20f70ce
ks-moss <k.sinamkam.usa@gmail.com>
2023-05-25 02:27:06 -0700

    Update

--- src/contact-script.js
+++ src/contact-script.js
@@ -4,15 +4,14 @@ var callAPI = (name, email, msg) => {
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
+    
     // Get Date and Time
     // Create a new Date object with the current time
     var currentDate = new Date();
-
     // Get the year, month, and day
     var year = currentDate.getFullYear();
     var month = currentDate.getMonth() + 1; // Adding 1 because the month is zero-based
     var day = currentDate.getDate();
-
     // Get the hours, minutes, seconds, and milliseconds in Pacific Time
     var ptOffset = -7; // Pacific Time offset from UTC (currently -7 for PDT, -8 for PST)
     var ptHours = currentDate.getUTCHours() + ptOffset;
@@ -20,17 +19,15 @@ var callAPI = (name, email, msg) => {
     var minutes = currentDate.getUTCMinutes();
     var seconds = currentDate.getUTCSeconds();
     var milliseconds = currentDate.getUTCMilliseconds();
-
     // Formatting single-digit values with leading zeros
     month = month < 10 ? '0' + month : month;
     day = day < 10 ? '0' + day : day;
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
-
     // Create the formatted date and time string
     var dateTime = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
-    
+
     // using built-in JSON utility package turn object to string and store in a variable
     var raw = JSON.stringify({"name": name, "email": email, "msg": msg, "now": dateTime});
     // create a JSON object with parameters for API call and store in a variable
