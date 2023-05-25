dd3d032155eab3114d40af3ab08431dd94a51b29
ks-moss <k.sinamkam.usa@gmail.com>
2023-05-25 01:33:53 -0700

    Update

--- src/contact-script.js
+++ src/contact-script.js
@@ -1,4 +1,5 @@
 // callAPI function that takes the base and exponent numbers as parameters
+ID = 0;
 var callAPI = (name, email, msg) => {
     // instantiate a headers object
     var myHeaders = new Headers();
@@ -7,10 +8,9 @@ var callAPI = (name, email, msg) => {
     
     // Get Date and Time
     var currentDateTime = new Date();
-    var milliseconds = currentDateTime.getMilliseconds();
-
+    ID++;
     // using built-in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({ "name": name, "email": email, "msg": msg, "now": milliseconds });
+    var raw = JSON.stringify({ "ID":ID,"name": name, "email": email, "msg": msg, "now": currentDateTime });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
