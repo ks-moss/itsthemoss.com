d0e60cec8dee99def1f0480a6019e0702afd052d
ks-moss <k.sinamkam.usa@gmail.com>
2023-05-25 01:36:56 -0700

    Update

--- src/contact-script.js
+++ src/contact-script.js
@@ -10,7 +10,7 @@ var callAPI = (name, email, msg) => {
     var currentDateTime = new Date();
     ID++;
     // using built-in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({ "ID":ID,"name": name, "email": email, "msg": msg, "now": currentDateTime });
+    var raw = JSON.stringify({ "id":ID,"name": name, "email": email, "msg": msg, "now": currentDateTime });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
