8caf5364e2c6190cc536d4e5f5c708ba8eb030b1
ks-moss <k.sinamkam.usa@gmail.com>
2023-05-25 01:22:49 -0700

    Update

--- src/contact-script.js
+++ src/contact-script.js
@@ -1,11 +1,11 @@
 // callAPI function that takes the base and exponent numbers as parameters
-var callAPI = (name,email,msg)=>{
+var callAPI = (name, email, msg) => {
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
-    // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"name":name,"email":email, "msg":msg, "now":new Date()});
+    // using built-in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({ "name": name, "email": email, "msg": msg, "now": new Date() });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
@@ -15,7 +15,7 @@ var callAPI = (name,email,msg)=>{
     };
     // make API call with parameters and use promises to get response
     fetch("https://qgetwedvkg.execute-api.ca-central-1.amazonaws.com/main", requestOptions)
-    .then(response => response.text())
-    .then(result => alert(JSON.parse(result).body))
-    .catch(error => console.log('error', error));
+        .then(response => response.text())
+        .then(result => alert(JSON.parse(result).body))
+        .catch(error => console.log('error', error));
 }
