c596fb74d83449b163d45fbd7c19bc13b946f03e
ks-moss <k.sinamkam.usa@gmail.com>
2023-05-25 02:18:27 -0700

    Update

--- src/contact-script.js
+++ src/contact-script.js
@@ -5,12 +5,28 @@ var callAPI = (name, email, msg) => {
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // Get Date and Time
-    // Get Date and Time
-    var currentDateTime = new Date();
-    var options = { timeZone: 'America/Los_Angeles' };
-    var pacificTime = currentDateTime.toLocaleString('en-US', options);
+    var currentDate = new Date();
+
+    var year = currentDate.getFullYear();
+    var month = currentDate.getMonth() + 1; // Adding 1 because the month is zero-based
+    var day = currentDate.getDate();
+
+    var hours = currentDate.getHours();
+    var minutes = currentDate.getMinutes();
+    var seconds = currentDate.getSeconds();
+    var milliseconds = currentDate.getMilliseconds();
+
+    // Formatting single-digit values with leading zeros
+    month = month < 10 ? '0' + month : month;
+    day = day < 10 ? '0' + day : day;
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    var dateTime = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds + '.' + milliseconds;
+
     // using built-in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"name": name, "email": email, "msg": msg, "now": pacificTime });
+    var raw = JSON.stringify({"name": name, "email": email, "msg": msg, "now": dateTime });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
