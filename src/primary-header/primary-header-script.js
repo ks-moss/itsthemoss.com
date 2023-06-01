e54a33c181b1703e115437c6ea0d67a843b4fd47
ks-moss <k.sinamkam.usa@gmail.com>
2023-06-01 04:33:53 -0700

    Update

---
+++ src/primary-header/primary-header-script.js
@@ -0,0 +1,16 @@
+// const div = document.getElementById('primaryHeaderContents');
+
+// // Fetch the external HTML file
+// fetch('primary-header/primary-header.html')
+//   .then(response => response.text())
+//   .then(data => {
+//     // Insert the HTML content into the specified div
+//     div.innerHTML = data;
+
+//     // Fetch the external JavaScript file
+//     const script = document.createElement('script');
+//     script.src = 'primary-header/primary-header-motion-script.js';
+
+//     // Append the script to the document body
+//     document.body.appendChild(script);
+//   });
